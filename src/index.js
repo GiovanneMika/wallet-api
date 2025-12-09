83644f184559b240ff04b099addde719dc9a497d
Giovanne-Vamilly <mika.2023@alunos.utfpr.edu.br>
2025-12-09 14:32:25 -0300

    Finalização do projeto

--- src/index.js
+++ src/index.js
@@ -25,6 +25,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
 
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, item) => {
+        if (item.type === "credit")
+            return acc + item.amount;
+        if (item.type === "debit")
+            return acc - item.amount;
+    }, 0);
+
+    return balance;
+
+}
+
 function verifyIfExistsAccountID(request, response, next) {
 
     const { id } = request.params;
@@ -42,7 +54,7 @@ function verifyIfExistsAccountID(request, response, next) {
 }
 
 function verifyIfAccountAlreadyExists(request, response, next) {
-    const { cpf } = request;
+    const { cpf } = request.body;
     const customerAlreadyExists = customers.some(customer => customer.cpf === cpf);
     if (customerAlreadyExists) {
         return response.status(403).json(
@@ -51,9 +63,10 @@ function verifyIfAccountAlreadyExists(request, response, next) {
             }
         );
     }
+    return next();
 }
 
-app.post("/account", (request, response) => {
+app.post("/account", verifyIfAccountAlreadyExists, (request, response) => {
     const { name, cpf } = request.body;
 
     const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
@@ -71,46 +84,6 @@ app.post("/account", (request, response) => {
     );
 })
 
-// function onSuccess() {
-//     customers.push({
-//         cpf,
-//         name,
-//         id,
-//         statemet: []
-//     });
-//     return response.status(201).send(
-//         {
-//             message: `Conta de ${name} criada!`,
-//         }
-//     );
-// }
-
-// function returnBrabo(ret) {
-//     return ret;
-// }
-
-// customerAlreadyExists ? onSuccess() : returnBrabo(response.status(403).json(
-//     {
-//         error: `Esse cpf ja está em uso!`,
-//     }
-// ));
-
-
-// if (!customers.find(customer => customer.cpf === cpf)) {
-//     customers.push({
-//         cpf,
-//         name,
-//         id,
-//         statemet: []
-//     });
-// } else {
-//     return response.status(403).send(
-//         {
-//             message: `Esse cpf ja está em uso!`,
-//         }
-//     );
-// }
-
 app.get("/accounts", (request, response) => {
     return response.send(customers);
 })
@@ -123,10 +96,110 @@ app.get("/account/:id", verifyIfExistsAccountID, (request, response) => {
     return response.json(customer);
 })
 
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+    return response.status(200).json(customer);
+})
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).json({
+        message: "Alteração feita com sucesso!"
+    })
+})
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response)=>{
+    const {customer} = request;
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+})
+
 
 app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
     const { customer } = request;
     return response.json(customer.statement);
 });
 
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+    const { date } = request.query;
+
+    const dateFormat = new Date(date + " 00:00");
+    console.log(dateFormat.toLocaleDateString());
+
+    const statement = customer.statement.filter(statement => statement.created_at.toLocaleDateString() === new Date(dateFormat).toLocaleDateString());
+
+
+    return response.json(statement);
+
+});
+
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).json({
+        message: `Depósito de R$${amount} realizado na conta de ${customer.name}`,
+        saldo: `${getBalance(customer.statement)}`
+    })
+})
+
+function verifyEnoughBalance(request, response, next) {
+    const { customer } = request;
+    const { amount } = request.body;
+
+    if (getBalance(customer.statement) - amount < 0) {
+        return response.status(400).json({
+            message: "Saldo insuficiente!"
+        })
+    }
+    return next();
+
+}
+
+app.post("/withdraw", verifyIfExistsAccountCPF, verifyEnoughBalance, (request, response) => {
+
+    const { customer } = request;
+    const { description, amount } = request.body;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).json({
+        message: `Débito de R$${amount} realizado na conta de ${customer.name}`,
+        saldo: `${getBalance(customer.statement)}`
+    })
+})
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+    const {customer} = request;
+    const balance = getBalance(customer.statement);
+
+    return response.status(200).json({
+        message: `O saldo atual desse cpf é de R$${balance}`,
+        statements: customer.statement
+    })
+})
 app.listen(3333);
